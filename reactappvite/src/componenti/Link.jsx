0350e9158531ab5851b53b403cfba0cf7724fbef
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-11-21 13:49:49 +0100

    reactvite

---
+++ reactappvite/src/componenti/Link.jsx
@@ -0,0 +1,10 @@
+import './Navbar.css'
+
+function Link() {
+    return <a href="#"> Er Link </a>
+        
+    
+
+}
+
+export default Link
---
+++ reactappvite/src/componenti/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+import Link from './Link'
+
+function Navbar() {
+    return(
+        <ul>
+            <li><Link></Link></li>
+            <li><a href="#"> Ciao </a></li>
+            <li><a href="#"> Ciao </a></li>
+            <li><Link></Link></li>
+            <li><a href="#"> Ciao </a></li>
+        </ul>
+    )
+
+}
+
+export default Navbar
